f3242e1135306877b5963b02b2e6f60cd872f683
Ted Mielczarek <ted@fullstory.com>
2021-12-22 13:13:43 -0500

    add a bottle

--- Formula/rustfilt.rb
+++ Formula/rustfilt.rb
@@ -4,6 +4,12 @@ class Rustfilt < Formula
   url "https://github.com/luser/rustfilt/archive/0.2.1.tar.gz"
   sha256 "f09bb822c8b22c4c89bf63cc64f8f85a053e1850a70cad4b7308e00871527496"
 
+  bottle do
+    root_url "https://github.com/luser/rustfilt/releases/download/0.2.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur: "692953e844b5728246443bd2faec687a17caa2c694209a2219614c041fb68226"
+  end
+
   depends_on "rust" => :build
 
   def install
